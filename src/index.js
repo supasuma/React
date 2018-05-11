750f16723e41ebb025e6089767cff8918074b9fb
supasuma <summer.brines@babylonhealth.com>
2018-05-11 08:53:59 +0100

    working tic tac toe game

--- src/index.js
+++ src/index.js
@@ -2,38 +2,60 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      //onClick prop on the built-in DOM <button> component tells React to set up a click event listener.
-      //When the button is clicked, React will call the onClick event handler defined in Square’s render() method.
-      //This event handler calls this.props.onClick(). Square’s props were specified by the Board.
-      //Board passed onClick={() => this.handleClick(i)} to Square, so, when called, it runs this.handleClick(i)
-      //on the Board.
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+//FUNCTIONAL COMPONENT - components that only consist of a render
+function Square(props) {
+  return (
+  //onClick prop on the built-in DOM <button> component tells React to set up a click event listener.
+  //When the button is clicked, React will call the onClick event handler defined in Square’s render() method.
+  //This event handler calls this.props.onClick(). Square’s props were specified by the Board.
+  //Board passed onClick={() => this.handleClick(i)} to Square, so, when called, it runs this.handleClick(i)
+  //on the Board.
+    <button className="square" onClick={props.onClick}>
+      {/* passing the function props.onClick down is enough and note that onClick={props.onClick()}
+      would not work because it would call props.onClick immediately instead of passing it down */}
+      {props.value}
+
+    </button>
+  )
 }
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state={
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
   //Note how whenever Board’s state changes, the Square components rerender automatically.
 
   handleClick(i) {
+    console.log("handleclick")
     //.slice() to copy the squares array instead of mutating the existing array (and removes from array)
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    // return if already won OR square already filled
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
+    console.log("render square")
     return (
       <Square
         //Square no longer keeps its own state; it receives its value from its parent Board
@@ -45,7 +67,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    console.log("board render method")
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -84,6 +113,32 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  console.log("calculateWinner")
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    // console.log(a, b, c)
+    // console.log(squares[a], squares[b], squares[c])
+    // console.log(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
